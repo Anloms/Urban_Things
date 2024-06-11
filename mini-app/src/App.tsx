0f5a779a5a843274cffd9db5f40b0ba677dbcec8
Oweley <aleksandra.smolna@gmail.com>
2024-06-11 10:00:06 +0100

    feat: Template scheleton

--- mini-app/src/App.tsx
+++ mini-app/src/App.tsx
@@ -1,9 +1,10 @@
 import './index.css'
 import Form from './components/Form'
-
+import Template from './components/Template'
 function App() {
   return (
     <main className='bg-slate-500 w-screen h-screen pt-[5rem]'>
+      <Template />
       <Form />
     </main>
   )
---
+++ mini-app/src/components/Template.tsx
@@ -0,0 +1,10 @@
+export default function Template() {
+  return (
+    <tbody>
+      <tr className="flex flex-row justify-between wmax-[15rem] mr-[1rem]">
+        <th className="pr-3">hello</th>
+        <td>there</td>
+      </tr>
+    </tbody>
+  );
+}
