d58049af8ac982d12ad9f61b37afde99324d616e
Oweley <aleksandra.smolna@gmail.com>
2024-06-11 09:56:55 +0100

    basic setup - vite, typescript, tailwind

---
+++ mini-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <main className='bg-slate-500 w-screen h-screen pt-[5rem]'>
+      <Form />
+    </main>
+  )
+}
+
+export default App
---
+++ mini-app/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/*.{html,js,jsx,ts,tsx}",
+    "./*.{html,js,ts,tsx,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
